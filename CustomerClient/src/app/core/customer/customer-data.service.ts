b5ee639f93edbb49ef8363880b681b57f0b9cab8
johnmcraig <jmcraig32884@gmail.com>
2019-05-18 12:33:02 -0400

    Created customer interface in shared dir

--- CustomerClient/src/app/core/customer/customer-data.service.ts
+++ CustomerClient/src/app/core/customer/customer-data.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Customer } from 'src/app/shared/models/customer';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class CustomerDataService {
   constructor(private readonly http: HttpClient) { }
 
   getAll() {
-    return this.http.get(`${environment.endpoint}${this.controllerEndpoint}`);
+    return this.http.get<Customer[]>(`${environment.endpoint}${this.controllerEndpoint}`);
   }
 }
---
+++ CustomerClient/src/app/shared/models/customer.ts
@@ -0,0 +1,6 @@
+export interface Customer {
+    name: String;
+    id: Number;
+    postion: String;
+    age: String;
+}
