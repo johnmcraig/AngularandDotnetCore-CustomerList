8a86212901a92352a43442db370b8cd4a5c8e514
johnmcraig <jmcraig32884@gmail.com>
2019-05-18 13:02:36 -0400

    scaffold components and containers

--- CustomerClient/src/app/app.module.ts
+++ CustomerClient/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { CustomerModule } from './customer/presentational/customer/customer.module';
+import { CustomersComponent } from './customer/container/customers/customers.component';
+import { CustomerListComponent } from './customer/presentational/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    CustomerListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CoreModule, CustomerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
