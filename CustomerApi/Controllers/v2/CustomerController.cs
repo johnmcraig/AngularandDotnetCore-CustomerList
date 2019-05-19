a71f0825ac418745c07fe2dc55cdb2b06d600a3a
johnmcraig <jmcraig32884@gmail.com>
2019-05-19 15:20:11 -0400

    Refactored unused namespaces

--- CustomerApi/Controllers/v2/CustomerController.cs
+++ CustomerApi/Controllers/v2/CustomerController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Controllers.Controllers
+namespace CustomerApi.Controllers.v2
 {
     [ApiVersion("2.0")]
     [Route("api/v{version:apiVersion}/[controller]")]
