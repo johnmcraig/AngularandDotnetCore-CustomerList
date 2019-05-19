1fc88ac0e0b60a91c4f9b8851dcd9416e8c26bc9
johnmcraig <jmcraig32884@gmail.com>
2019-05-19 12:06:39 -0400

    Removed unused declartions

--- CustomerClient/src/app/app.module.ts
+++ CustomerClient/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { CustomerModule } from './customer/customer.module';
-import { CustomersComponent } from './customer/container/customers/customers.component';
-import { CustomerListComponent } from './customer/presentational/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CustomersComponent,
-    CustomerListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
