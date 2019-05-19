15c1bcd7a80e0e2ad815120b56d3caec5af5ff4e
johnmcraig <jmcraig32884@gmail.com>
2019-05-19 12:05:11 -0400

    Added customer-list binding

--- CustomerClient/src/app/customer/customer.module.ts
+++ CustomerClient/src/app/customer/customer.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { allPresentationalComponents } from './presentational';
-import { allContainerComponents } from './container';
+import { allPresentationalComponents } from './presentational/index';
+import { allContainerComponents } from './container/index';
 import { CustomerFormComponent } from './presentational/customer-form/customer-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
