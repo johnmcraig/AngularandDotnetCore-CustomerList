7f82bdf26ef79953e7222b6201151a0ef3ed36c7
johnmcraig <jmcraig32884@gmail.com>
2019-05-20 14:16:06 -0400

    Added action methods to v2 controller with db

--- CustomerApi/Controllers/v2/CustomerController.cs
+++ CustomerApi/Controllers/v2/CustomerController.cs
@@ -1,4 +1,9 @@
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerApi.Data;
+using CustomerApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomerApi.Controllers.v2
 {
@@ -7,10 +12,89 @@ namespace CustomerApi.Controllers.v2
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult Gets()
+        private readonly CustomerDbContext _dbContext;
+
+        public CustomerController(CustomerDbContext dbContext)
         {
-            return Ok("This is version 2.0 of the customer controller");
+            _dbContext = dbContext;
+
+            if(_dbContext.Customers.Count() == 0)
+                _dbContext.Add(new Customer
+                {
+                    Name = "Bob", 
+                    Age = 40, 
+                    Position = "Sales Manager"
+                });
+                _dbContext.SaveChanges();
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Customer>> GetAll()
+        {
+            var customers = await _dbContext.Customers.ToListAsync();
+            
+            if(customers == null)
+                return NotFound();
+
+            return Ok(customers);
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<Customer>> GetSingle(int id)
+        {
+            var customer = await _dbContext.Customers.FirstOrDefaultAsync(x => x.Id == id);
+
+            if( customer == null)
+                return NotFound();
+
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Customer>> AddCustomer([FromBody] Customer customer)
+        {
+             await _dbContext.Customers.AddAsync(customer);
+             await _dbContext.SaveChangesAsync();
+
+            if(customer == null)
+                return BadRequest("Customer could not be added");
+
+            return CreatedAtAction(nameof(GetSingle), 
+                new
+                { 
+                    id = customer.Id,
+                    name = customer.Name,
+                    position = customer.Position,
+                    age = customer.Age 
+                },
+                customer);
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+        {
+            if( id != customer.Id)
+                return BadRequest();
+
+            _dbContext.Entry(customer).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var customer = await _dbContext.Customers.FindAsync(id);
+
+            if( customer == null)
+                return NotFound();
+
+            _dbContext.Customers.Remove(customer);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
