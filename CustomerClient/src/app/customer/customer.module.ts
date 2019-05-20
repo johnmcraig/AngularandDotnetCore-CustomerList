4099de3c687eca85a855ade0d7f72267ccb83591
johnmcraig <jmcraig32884@gmail.com>
2019-05-19 21:45:59 -0400

    Generated customer-detail component

--- CustomerClient/src/app/customer/customer.module.ts
+++ CustomerClient/src/app/customer/customer.module.ts
@@ -4,9 +4,10 @@ import { allPresentationalComponents } from './presentational/index';
 import { allContainerComponents } from './container/index';
 import { CustomerFormComponent } from './presentational/customer-form/customer-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CustomerDetailComponent } from './presentational/customer-detail/customer-detail.component';
 
 @NgModule({
-  declarations: [...allPresentationalComponents, ...allContainerComponents, CustomerFormComponent],
+  declarations: [...allPresentationalComponents, ...allContainerComponents, CustomerFormComponent, CustomerDetailComponent],
   imports: [
     CommonModule, ReactiveFormsModule
   ],
