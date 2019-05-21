3cca4dc1928d501d7911c083d5fe3375648812b7
johnmcraig <jmcraig32884@gmail.com>
2019-05-21 01:40:49 -0400

    Corrected module name and reordered paths in route

--- CustomerClient/src/app/app-route.module.ts
+++ CustomerClient/src/app/app-route.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CustomerDetailComponent } from './customer/presentational/customer-detail/customer-detail.component';
+import { CustomerDetailsComponent } from './customer/presentational/customer-details/customer-details.component';
 import { CustomersComponent } from './customer/container/customers/customers.component';
 
 const routes: Routes = [
-    { path: 'detail/:id', component: CustomerDetailComponent },
     { path: 'home', component: CustomersComponent },
+    { path: 'details/:id', component: CustomerDetailsComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
 ];
