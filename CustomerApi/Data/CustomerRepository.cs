53fed3ce1218a72de30322ca6510131be707d04a
johnmcraig <jmcraig32884@gmail.com>
2019-07-07 12:29:04 -0400

    Modified repository to access data layer

--- CustomerApi/Data/CustomerRepository.cs
+++ CustomerApi/Data/CustomerRepository.cs
@@ -1,63 +1,56 @@
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.Threading.Tasks;
 using CustomerApi.Models;
 
 namespace CustomerApi.Data
 {
     public class CustomerRepository : ICustomerRepository
     {
-        /// <summary>
-        /// A repository pattern to store a list of customers in data as a dictionary
-        /// </summary>
-        /// <typeparam name="int"></typeparam>
-        /// <typeparam name="Customer"></typeparam>
-        /// <returns></returns>
-        private readonly ConcurrentDictionary<int, Customer> _storage = new ConcurrentDictionary<int, Customer>();
+        private readonly CustomerDbContext _dbContext;
+
+        public CustomerRepository(CustomerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
         public Customer GetSingle(int id)
         {
-            return _storage.TryGetValue(id, out var item) ? item : null;
+            return _dbContext.Customers.FirstOrDefault(x => x.Id == id);
         }
 
         public void Add(Customer item)
         {
-            item.Id = !_storage.Values.Any() ? 1 : _storage.Values.Max(x => x.Id) + 1;
-
-            if(!_storage.TryAdd(item.Id, item))
-            {
-                throw new Exception("Item could not be added");
-            }
+            _dbContext.Customers.Add(item);
         }
 
         public void Delete(int id)
         {
-            if(!_storage.TryRemove(id, out var item))
-            {
-                throw new Exception("Item could not be removed");
-            }
+            Customer customer = GetSingle(id);
+            _dbContext.Customers.Remove(customer);
         }
 
         public Customer Update(int id, Customer item)
         {
-            _storage.TryUpdate(id, item, GetSingle(id));
+            _dbContext.Customers.Update(item);
             return item;
         }
 
         public IQueryable<Customer> GetAll()
         {
-            IQueryable<Customer> _allItems = _storage.Values.AsQueryable();
+            IQueryable<Customer> _allItems = _dbContext.Customers.OrderBy(x => x.Name);
             return _allItems;
         }
 
         public int Count()
         {
-            return _storage.Count;
+            return _dbContext.Customers.Count();
         }
 
         public bool Save()
         {
-            return true;
+            return _dbContext.SaveChanges() >= 0;
         }
     }
 }
