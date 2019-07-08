8809549e12b7e14c04d2aee52486636a9e365996
johnmcraig <jmcraig32884@gmail.com>
2019-07-08 00:22:59 -0400

    Made exported components easier to read

--- CustomerClient/src/app/customer/presentational/index.ts
+++ CustomerClient/src/app/customer/presentational/index.ts
@@ -1,3 +1,13 @@
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerDeleteComponent } from './customer-delete/customer-delete.component';
+import { CustomerUpdateComponent } from './customer-update/customer-update.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
 
-export const allPresentationalComponents = [CustomerListComponent];
+export const allPresentationalComponents = [
+    CustomerListComponent,
+    CustomerDetailsComponent,
+    CustomerDeleteComponent,
+    CustomerUpdateComponent,
+    CustomerFormComponent
+];
