d7f5cc5b3d0580b84fa990e594a01e6f0ea9927c
johnmcraig <jmcraig32884@gmail.com>
2019-07-16 23:40:18 -0400

    Oredered interface properties to match

--- src/Customer-List/server/Data/ICustomerRepository.cs
+++ src/Customer-List/server/Data/ICustomerRepository.cs
@@ -7,10 +7,12 @@ namespace CustomerApi.Data
     {
         void Add(Customer item);
         void Delete(int id);
+
+        IQueryable<Customer> GetAll();
         Customer Update(int id, Customer item);
         Customer GetSingle(int id);
+        
         bool Save();
-        IQueryable<Customer> GetAll();
         int Count();
     }
 }
